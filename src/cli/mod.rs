c9a0a72b149158ae829b1034a6ce3878718f5c9e
Yury Antonov <1390348+yantonov@users.noreply.github.com>
2021-04-10 07:35:07 +0200

    port number can be parameterized

---
+++ src/cli/mod.rs
@@ -0,0 +1,48 @@
+use clap::{Clap, crate_version};
+
+#[derive(Clap)]
+#[clap(version = crate_version ! ())]
+struct Opts {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Clap)]
+pub enum Command {
+    #[clap(about = "run notification server", display_order = 0)]
+    Server(Server)
+}
+
+#[derive(Clap)]
+pub struct Server {
+    #[clap(about = "port number (default port = 4242)", short, long, )]
+    port: Option<u16>,
+}
+
+impl Server {
+    pub fn port(&self) -> Result<u16, String> {
+        let default_port: u16 = 4242;
+        let port: u16 = self.port
+            .unwrap_or(default_port);
+        let min_port = 1;
+        let max_port = 65535;
+        if port < min_port || port > max_port {
+            return Err(format!("port number should be between {} and {}", min_port, max_port));
+        }
+        Ok(port)
+    }
+}
+
+pub struct Arguments {
+    args: Opts
+}
+
+impl Arguments {
+    pub fn command(&self) -> &Command {
+        &self.args.command
+    }
+}
+
+pub fn arguments() -> Arguments {
+    return Arguments { args: Opts::parse() };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
+mod cli;
+
 use warp::Filter;
+use cli::Command;
+
+async fn entry_point() -> Result<(), String> {
+    let arguments = cli::arguments();
+    match arguments.command() {
+        Command::Server(server) => {
+            let port_number = server.port()?;
+            // GET /hello/warp => 200 OK with body "Hello, warp!"
+            let hello = warp::path!("hello" / String)
+                .map(|name| format!("Hello, {}!", name));
+
+            Ok(warp::serve(hello)
+                .run(([127, 0, 0, 1], port_number))
+                .await)
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() {
-    // GET /hello/warp => 200 OK with body "Hello, warp!"
-    let hello = warp::path!("hello" / String)
-        .map(|name| format!("Hello, {}!", name));
-
-    warp::serve(hello)
-        .run(([127, 0, 0, 1], 3030))
-        .await;
+    match entry_point().await {
+        Ok(_) => std::process::exit(0),
+        Err(message) => {
+            eprintln!("{}", message);
+            std::process::exit(1);
+        }
+    }
 }
